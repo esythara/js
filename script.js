d9816ed192c6c2f46e31f8396142b0abdde2d79e
e2y <43453213+esythara@users.noreply.github.com>
2022-05-02 16:57:19 +0100

    second commit
    Sliding through images via JQuery

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 $(function () {
+  var galleryImage = $(".gallery").find("img").first();
   var images = [
-    "images/laptop-mobile_small.jpg",
+    "images/laptop-moblie_small.jpg",
     "images/laptop-on-table_small.jpg",
     "images/people-office-group-team_small.jpg",
   ];
 
   var i = 0;
-  $(".gallery").find("img").on("click", function () {
+  setInterval(function () {
     i = (i + 1) % images.length;
-    $(this).fadeOut(function () {
-      $(this).attr("src", images[i]).fadeIn();
+    galleryImage.fadeOut(function () {
+      $(this).attr("src", images[i]);
+      $(this).fadeIn();
     });
-  });
+    console.log(galleryImage.attr("src"));
+  }, 4000);
 });
