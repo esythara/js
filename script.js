d776726fbdb4c6f3a5f4b221659f5f0379b6fc90
e2y <43453213+esythara@users.noreply.github.com>
2022-05-02 16:56:22 +0100

    initial commit
    on-click ability to sift through images

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function () {
+  var images = [
+    "images/laptop-mobile_small.jpg",
+    "images/laptop-on-table_small.jpg",
+    "images/people-office-group-team_small.jpg",
+  ];
+
+  var i = 0;
+  $(".gallery").find("img").on("click", function () {
+    i = (i + 1) % images.length;
+    $(this).fadeOut(function () {
+      $(this).attr("src", images[i]).fadeIn();
+    });
+  });
+});
